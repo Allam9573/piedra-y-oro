eb9a9e036c59e0a3717d15bb4e6315d15d8e8a9c
allam9573 <aallamdaniel@gmail.com>
2024-11-18 15:06:33 -0600

    algo

--- src/pages/Categorias.jsx
+++ src/pages/Categorias.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { TablaCategorias } from "../components/categorias/TablaCategorias"
+
 export const Categorias = () => {
     return (
         <div className='container py-5'>

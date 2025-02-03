532836862815176ff34ef988556118a755ba4662
allam9573 <aallamdaniel@gmail.com>
2025-02-03 15:42:33 -0600

    prueba imagenes

--- src/components/categorias/HomeCategorias.jsx
+++ src/components/categorias/HomeCategorias.jsx
@@ -7,13 +7,15 @@ import { Link } from 'react-router-dom'
 import '../../assets/css/styles.css'
 import anillo from '../../assets/img/anillo.jpeg'
 import pulsera from '../../assets/img/pulsera1.jpeg'
+import modelo1 from '../../assets/img/modelo1.jpg'
+import modelo2 from '../../assets/img/modelo2.jpg'
 
 export const HomeCategorias = () => {
     return (
         <section className="container py-5">
             <div className="row text-center py-3">
                 <div className="col-lg-6 m-auto">
-                    <h1 className="h1" style={{ color: '#C7D0CF' }}>Comprar por Categorias</h1>
+                    <h1 className="h1" style={{ color: '#343A36' }}>Comprar por Categorias</h1>
                 </div>
             </div>
             <div className="container">
@@ -21,15 +23,15 @@ export const HomeCategorias = () => {
                     {/* Joyeria */}
                     <div className="col-3 col-md-3 text-center p-3">
                         <div className='position-relative h-100 w-100'>
-                            <img src={pulsera} className='img-fluid' alt="" />
+                            <img src={modelo1} className='img-fluid' alt="" />
                             <Link to={`/productos?categoria=Pulseras`} style={{ backgroundColor: '#E7D7C9' }} className='btn position-absolute top-50 start-50 translate-middle'>Pulseras</Link>
                         </div>
                     </div>
 
                     {/* Niños */}
                     <div className="col-3 col-md-3 text-center p-3">
-                        <div className='position-relative h-100 w-100 bg-primary'>
-                            <img src={anillo} className='img-fluid' alt="" />
+                        <div className='position-relative h-100 w-100'>
+                            <img src={modelo2} className='img-fluid' alt="" />
                             <Link to={`/productos?categoria=Aretes`} style={{ backgroundColor: '#E7D7C9' }} className='btn position-absolute top-50 start-50 translate-middle'>Aretes</Link>
                         </div>
                     </div>

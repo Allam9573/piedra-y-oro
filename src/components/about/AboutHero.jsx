8f408da1fef11c024aad7f34fea65e818b258500
allam9573 <aallamdaniel@gmail.com>
2025-02-06 10:53:16 -0600

    prueba home categorias

--- src/components/about/AboutHero.jsx
+++ src/components/about/AboutHero.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import image from '../../assets/img/about-hero.svg'
+import image from '../../assets/img/about.svg'
 
 export const AboutHero = () => {
     return (
-        <section class="bg-success py-5">
+        <section class="py-5">
             <div class="container">
                 <div class="row align-items-center py-5">
-                    <div class="col-md-8 text-white">
-                        <h1>Quienes Somos</h1>
-                        <p>
+                    <div class="col-md-8 mt-5 p-5">
+                        <h1 className='text-brown mb-5'>Quienes Somos</h1>
+                        <p className='text-brown'>
                             Somos una joyería hondureña dedicada a empoderar a las mujeres a través de nuestros diseños, inspirados en la belleza única y radiante de la feminidad. Queremos transmitir sensaciones que perduren, ofreciendo piezas que no solo resalten tu estilo, sino también tu esencia.
 
                             Además de nuestras colecciones modernas y sofisticadas, también ofrecemos piezas artesanales diseñadas para acompañarte en los momentos más importantes de la vida de tus seres queridos, como medallas de cuna o bendiciones de hogar. Estas joyas especiales son creaciones llenas de significado, pensadas para celebrar las etapas más significativas y memorables.
@@ -18,7 +18,7 @@ export const AboutHero = () => {
                         </p>
                     </div>
                     <div class="col-md-4">
-                        <img src={image} alt="About Hero" />
+                        <img src={image} width={'100%'} alt="About Hero" />
                     </div>
                 </div>
             </div>
--- src/services/HomeCategoriasService.js
+++ src/services/HomeCategoriasService.js
@@ -2,10 +2,13 @@ import axios from "axios";
 
 class HomeCategoriasService {
     getAnillos() {
-        return axios.get('http://localhost:8000/api/home/anillos/')
+        // return axios.get('http://localhost:8000/api/home/anillos/')
+        return axios.get('https://lionettas-backend-production.up.railway.app/api/home/anillos/')
+
     }
     getMedallones() {
-        return axios.get('http://localhost:8000/api/home/medallones/')
+        // return axios.get('http://localhost:8000/api/home/medallones/')
+        return axios.get('https://lionettas-backend-production.up.railway.app/api/home/medallones/')
     }
 }
 export default new HomeCategoriasService();

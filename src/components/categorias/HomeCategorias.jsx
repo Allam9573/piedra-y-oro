b3d5a47b8aa4f248d3ac7e620b336a78810beaac
allam9573 <aallamdaniel@gmail.com>
2025-02-07 12:16:38 -0600

    cambio de imagenes en home categorias

--- src/components/categorias/HomeCategorias.jsx
+++ src/components/categorias/HomeCategorias.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useCategorias } from '../../hooks/useCategorias'
 import joya from '../../assets/img/joyas.png'
 import ninos from '../../assets/img/ninos.png'
@@ -9,49 +9,61 @@ import anillo from '../../assets/img/anillo.jpeg'
 import pulsera from '../../assets/img/pulsera1.jpeg'
 import modelo1 from '../../assets/img/modelo1.jpg'
 import modelo2 from '../../assets/img/modelo2.jpg'
+import anillo1 from '../../assets/img/categorias/anillo1.jpg'
+import anillo2 from '../../assets/img/categorias/anillo2.jpg'
+import cadena1 from '../../assets/img/categorias/cadena1.jpg'
+import cadena2 from '../../assets/img/categorias/cadena2.jpg'
+import arete1 from '../../assets/img/categorias/arete1.jpg'
+import arete2 from '../../assets/img/categorias/arete2.jpg'
+import pulsera1 from '../../assets/img/categorias/pulsera1.jpg'
+import pulsera2 from '../../assets/img/categorias/pulsera2.jpg'
 
 export const HomeCategorias = () => {
+    const [hovered, setHovered] = useState({});
+    const categories = [
+        {
+            name: "PULSERAS",
+            image1: pulsera1,
+            image2: pulsera2
+        },
+        {
+            name: "ARETES",
+            image1: arete1,
+            image2: arete2
+        },
+        {
+            name: "CADENAS",
+            image1: cadena1,
+            image2: cadena2
+        },
+        {
+            name: "ANILLOS",
+            image1: anillo1,
+            image2: anillo2
+        },
+    ];
     return (
-        <section className="container py-5">
-            <div className="row text-center py-3">
-                <div className="col-lg-6 m-auto">
-                    <h1 className="h1" style={{ color: '#343A36' }}>Comprar por Categorias</h1>
-                </div>
+        <div className="container text-center py-5">
+            <h2 className="mb-4">Comprar por categoría</h2>
+            <div className="row">
+                {categories.map((category, index) => (
+                    <div
+                        key={index}
+                        className="col-12 col-md-6 col-lg-3 position-relative category-card"
+                        onMouseEnter={() => setHovered(index)}
+                        onMouseLeave={() => setHovered(null)}
+                    >
+                        <img
+                            src={hovered === index ? category.image2 : category.image1}
+                            alt={category.name}
+                            className="img-fluid rounded category-image"
+                        />
+                        <div className="position-absolute top-50 start-50 translate-middle bg-light px-4 py-2 fw-bold category-button">
+                            {category.name}
+                        </div>
+                    </div>
+                ))}
             </div>
-            <div className="container">
-                <div className="row">
-                    {/* Joyeria */}
-                    <div className="col-3 col-md-3 text-center p-3">
-                        <div className='position-relative h-100 w-100'>
-                            <img src={modelo1} className='img-fluid' alt="" />
-                            <Link to={`/productos?categoria=Pulseras`} style={{ backgroundColor: '#E7D7C9' }} className='btn position-absolute top-50 start-50 translate-middle'>Pulseras</Link>
-                        </div>
-                    </div>
-
-                    {/* Niños */}
-                    <div className="col-3 col-md-3 text-center p-3">
-                        <div className='position-relative h-100 w-100'>
-                            <img src={modelo2} className='img-fluid' alt="" />
-                            <Link to={`/productos?categoria=Aretes`} style={{ backgroundColor: '#E7D7C9' }} className='btn position-absolute top-50 start-50 translate-middle'>Aretes</Link>
-                        </div>
-                    </div>
-
-                    {/* Religión */}
-                    <div className="col-3 col-md-3 text-center p-3">
-                        <div className='position-relative h-100 w-100 bg-primary'>
-                            <img src={anillo} className='img-fluid' alt="" />
-                            <Link to={`/productos?categoria=Cadenas`} style={{ backgroundColor: '#E7D7C9' }} className='btn position-absolute top-50 start-50 translate-middle'>Cadenas</Link>
-                        </div>
-                    </div>
-                    <div className="col-3 col-md-3 text-center p-3">
-                        <div className='position-relative h-100 w-100 bg-primary'>
-                            <img src={anillo} className='img-fluid' alt="" />
-                            <Link to={`/productos?categoria=Anillos`} style={{ backgroundColor: '#E7D7C9' }} className='btn position-absolute top-50 start-50 translate-middle'>Anillos</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </section>
+        </div>
     )
 }

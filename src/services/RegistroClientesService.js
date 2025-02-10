38e758e4cb361057b379e97b56151b74c7c912a0
allam9573 <aallamdaniel@gmail.com>
2025-02-10 15:29:06 -0600

    correcion service url registro clientes v2

--- src/services/RegistroClientesService.js
+++ src/services/RegistroClientesService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class RegistroClientesService {
 
     addCliente(cliente) {
-        return axios.post(`https://lionettas-backend-production.up.railway.app/fwefewfwen/api/clientes-registrados/`, cliente)
+        return axios.post(`https://lionettas-backend-production.up.railway.app/api/clientes-registrados/`, cliente)
     }
 
 }

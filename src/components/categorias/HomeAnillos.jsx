24c12fd06d2275467b1bfff945684766df978cc6
allam9573 <aallamdaniel@gmail.com>
2025-03-11 15:44:51 -0600

    home anillos mobile

--- src/components/categorias/HomeAnillos.jsx
+++ src/components/categorias/HomeAnillos.jsx
@@ -57,12 +57,12 @@ export const HomeAnillos = () => {
                     {
                         infoAnillos.map(info => (
                             <>
-                                <h2 className='title-categories-mobile text-white'>{info.titulo}</h2>
+                                <h2 className='title-categories-mobile'>{info.titulo}</h2>
                                 <div className='d-flex justify-content-center mt-3'>
                                     <img className='home-anillos-home-imagen' src={anillo1} alt="" />
                                     <img className='home-anillos-home-imagen' src={anillo2} alt="" />
                                 </div>
-                                <p className='text-white mt-4 descripcion-home-anillos'>{info.descripcion}</p>
+                                <p className='mt-4 descripcion-home-anillos'>{info.descripcion}</p>
                                 <Link to={`/productos?categoria=Anillos`} className='btn-home-mobile'>Explora el amor y el compromiso</Link>
                             </>
                         ))

d619eff55beaa91372771b6f47df74eba33bd875
allam9573 <aallamdaniel@gmail.com>
2025-03-25 16:43:41 -0600

    config menu collpase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,16 @@ import '../assets/css/templatemo.css'
 import '../assets/js/bootstrap.bundle.min.js'
 
 export const Navbar = ({ quantity, quantityFavorites }) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
     return (
         <>
             <nav className="navbar-desktop navbar fixed-top navbar-expand-lg navbar-light shadow" style={{ backgroundColor: "#E7D7C9", width: "100%" }}>
@@ -75,58 +85,68 @@ export const Navbar = ({ quantity, quantityFavorites }) => {
             </nav>
 
             <nav className="navbar-mobile px-0 navbar fixed-top navbar-expand-lg navbar-light shadow" style={{ backgroundColor: "#E7D7C9", width: "100%" }}>
-                <div className="container d-flex justify-content-between align-items-center">
-                    <div className="d-flex justify-content-center align-items-center">
-                        <button className="navbar-toggler border-0 ms-auto" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#templatemo_main_nav" aria-controls="templatemo_main_nav" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <Link className="navbar-brand text-success text-center logo h1" to={'/'}>
-                            <img src={logo_blanco} className="img-fluid ms-4" style={{ maxWidth: '90%' }} alt="Logo" />
-                        </Link>
-                        <div className="mx-3 align-self-center flex-row d-flex">
-                            <Link className="nav-icon position-relative text-decoration-none" to={'/my-cart'}>
-                                <MdAddShoppingCart className='fs-3 link' />
-                                {quantity > 0 ? (
-                                    <span className="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-danger text-white">{quantity}</span>
-                                ) : (
-                                    <span className="position-absolute top-0 left-100 translate-middle badge rounded-pill text-white"></span>
-                                )}
-                            </Link>
-                            <Link className="nav-icon position-relative text-decoration-none" to={'/my-favorites'}>
-                                <MdFavoriteBorder className='fs-3 link' />
-                                {quantityFavorites > 0 ? (
-                                    <span className="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-danger text-white">{quantityFavorites}</span>
-                                ) : (
-                                    <span className="position-absolute top-0 left-100 translate-middle badge rounded-pill text-white"></span>
-                                )}
-                            </Link>
-                            <a className="nav-icon position-relative text-decoration-none" href="#">
-                                <i className="fa fa-fw fa-user text-dark mr-3"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="collapse navbar-collapse flex-fill d-lg-flex justify-content-lg-between"
-                        id="templatemo_main_nav">
-                        <div className="flex-fill">
-                            <ul className="nav navbar-nav d-flex justify-content-between align-items-center mx-lg-auto">
-                                <li className="nav-item">
-                                    <Link className="link" to={'/'}>Inicio</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="link" to={'/productos'}>Productos</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="link" to={'/nosotros'}>Nosotros</Link>
-                                </li>
-                            </ul>
-                        </div>
+      <div className="container d-flex justify-content-between align-items-center">
+        <div className="d-flex justify-content-center align-items-center" onClick={toggleMenu}>
+          <button className="navbar-toggler border-0 ms-auto" type="button" onClick={toggleMenu}>
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <Link className="navbar-brand text-success text-center logo h1" to={'/'}>
+            <img src={logo_blanco} className="img-fluid ms-4" style={{ maxWidth: '90%' }} alt="Logo" />
+          </Link>
+          <div className="mx-3 align-self-center flex-row d-flex">
+            <Link className="nav-icon position-relative text-decoration-none" to={'/my-cart'} onClick={closeMenu}>
+              <MdAddShoppingCart className='fs-3 link' />
+              {quantity > 0 && (
+                <span className="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-danger text-white">{quantity}</span>
+              )}
+            </Link>
+            <Link className="nav-icon position-relative text-decoration-none" to={'/my-favorites'} onClick={closeMenu}>
+              <MdFavoriteBorder className='fs-3 link' />
+              {quantityFavorites > 0 && (
+                <span className="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-danger text-white">{quantityFavorites}</span>
+              )}
+            </Link>
+            <a className="nav-icon position-relative text-decoration-none" href="#">
+              <i className="fa fa-fw fa-user text-dark mr-3"></i>
+            </a>
+          </div>
+        </div>
 
-                    </div>
-                </div>
-            </nav>
+        <div className={`mobile-menu ${isOpen ? 'open' : ''} w-100 d-lg-none`} style={{ backgroundColor: "#E7D7C9", position: 'absolute', top: '100%', left: 0, zIndex: 1000 }}>
+          <div className="flex-fill">
+            <ul className="nav navbar-nav d-flex flex-column align-items-center mx-lg-auto">
+              <li className="nav-item">
+                <Link className="link py-2" to={'/'} onClick={closeMenu}>Inicio</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="link py-2" to={'/productos'} onClick={closeMenu}>Productos</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="link py-2" to={'/nosotros'} onClick={closeMenu}>Nosotros</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
 
+        {/* Esta es la parte del menú de escritorio, que se mostrará en pantallas más grandes */}
+        <div className="collapse navbar-collapse flex-fill d-none d-lg-flex justify-content-lg-between"
+          id="templatemo_main_nav">
+          <div className="flex-fill">
+            <ul className="nav navbar-nav d-flex justify-content-between align-items-center mx-lg-auto">
+              <li className="nav-item">
+                <Link className="link" to={'/'}>Inicio</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="link" to={'/productos'}>Productos</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="link" to={'/nosotros'}>Nosotros</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
         </>
 
 
